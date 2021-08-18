e031304d7c343ebaf93fa010e1518a3b3ccbea3f
lokesh pemmanaboina <84330548+lokesh182002@users.noreply.github.com>
2021-08-18 21:38:01 +0530

    Add files via upload

---
+++ armstrong using a module.py
@@ -0,0 +1,9 @@
+#python script for checking armstrong number or not using module.
+n=int(input("enter a number:"))
+def nod(n):
+    while(n!=0):
+        no=no+1
+        n=n//10
+    return no
+import armstrong as a
+a.arm()
